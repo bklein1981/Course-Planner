db34684d739f0a2bb41e3a5e40b217e022a7e497
Brian Klein <bklein1981@gmail.com>
2023-11-06 21:25:12 -0600

    implemented user model

---
+++ server/models/User.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({ 
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  first_name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  profile: [
+    {
+      biography: {type: String},
+      skills: [{type: String,}],
+      links: [{type: String,}],
+      subject: {
+        type: Schema.Types.ObjectId,
+        ref: 'Focus"'
+      }
+    } 
+  ]
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
