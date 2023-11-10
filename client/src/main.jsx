2c4a86c9b20a0b98aaf09831be3c0849e9ab6e7c
Brian Klein <bklein1981@gmail.com>
2023-11-09 19:30:29 -0600

    replaced main.jsx and app.jsx with starter code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,23 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Home from './pages/Home'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
