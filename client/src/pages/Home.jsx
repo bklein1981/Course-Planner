b7d6b2d8513e24466b92317b02a8ec680b3bf919
jmcshan1 <jmcshan01@gmail.com>
2023-11-09 19:36:03 -0600

    added tailwind to react

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,9 @@ function Home() {
   
     return (
       <div>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
         <div>Logo</div>
         
         <div>Login or Sign Up Box</div>
