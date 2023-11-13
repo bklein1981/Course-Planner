315210b3e71a1336de57096c453cfb7dd0f5b2c6
jmcshan1 <jmcshan01@gmail.com>
2023-11-12 23:30:35 -0600

    l

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import Header from './components/Header.jsx';
 import Home from './pages/Home.jsx';
 import Navbar from './components/Navbar.jsx';
+import Login from './pages/Login.jsx'
+import Footer from './components/Footer.jsx'
 function App() {
 
 
   return (
     <>
       <Header />
-      <Home />
+      <Login />
+      <Footer />
     </>
   )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,30 +1,45 @@
-// The props variable is an object containing the welcomeClass helper method on the clickHandler property
-function Greeting(props) {
-    const welcomeStudent = () => {
-      alert(`Welcome, ${props.name}!`)
-    }
-  
-    return (
-      <div className="container text-center">
-        <div className="list-group">
-          {/* In React, we can directly attach event listeners to the to the relevant markup */}
-          <button 
-            onClick={props.clickHandler} 
-            className="list-group-item list-group-item-action list-group-item-info" 
-            aria-current="true"
-          >
-            Greet the Students!
-          </button>
-          <button 
-            onClick={welcomeStudent} 
-            className="list-group-item list-group-item-action"
-          >
-            {props.name}
-          </button>
-        </div>
+import Component  from "react";
+function Login() {
+
+  return (
+    <section className="text-gray-600 body-font">
+    <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+        <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
       </div>
-    );
-  }
-  
-  export default Greeting;
-  
+      <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center space-y-1 ">
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Login</h1>
+        <div className="flex w-full md:justify-start justify-center items-end">
+          <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4">
+            <label htmlFor="hero-field" className="leading-7 text-sm text-gray-600">Email</label>
+            <input type="text" id="hero-field" name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <label htmlFor="hero-field" className="leading-7 text-sm text-gray-600">Password</label>
+            <input type="text" id="hero-field" name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+
+          </div>
+          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+        </div>
+        <div></div>
+        <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Sign Up</h1>
+        <div className="flex w-full md:justify-start justify-center items-end">
+          <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4">
+            <label htmlFor="hero-field" className="leading-7 text-sm text-gray-600">First Name</label>
+            <input type="text" id="hero-field" name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <label htmlFor="hero-field" className="leading-7 text-sm text-gray-600">Last Name</label>
+            <input type="text" id="hero-field" name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <label htmlFor="hero-field" className="leading-7 text-sm text-gray-600">Email</label>
+            <input type="text" id="hero-field" name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+            <label htmlFor="hero-field" className="leading-7 text-sm text-gray-600">Password</label>
+            <input type="text" id="hero-field" name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-indigo-200 focus:bg-transparent focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+          </div>
+          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+        </div>
+        <p className="text-sm mt-2 text-gray-500 mb-8 w-full">Neutra shabby chic ramps, viral fixie.</p>
+        
+      </div>
+    </div>
+  </section>
+  );
+}
+
+export default Login;
