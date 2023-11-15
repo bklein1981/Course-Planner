919ea6870a3f2d836dbb36f5903d76573d994ed4
Brian Klein <bklein1981@gmail.com>
2023-11-15 10:00:38 -0600

    updated layout of elements. Changed bio section to table to ensure continuity between titles and data. Moved add subject button to below profile section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
       setUserId(loggedInUserId);
       console.log(loggedInUserId)
 
-    } 
+    }
   }, []);
 
 
@@ -31,7 +31,7 @@ function Header() {
       refetch({ userId }); // Re-fetch the user data with the updated userId
     }
   }, [userId, refetch]);
-   
+
   useEffect(() => {
     if (userId && data) {
       console.log("User Data:", data);
@@ -40,7 +40,7 @@ function Header() {
       setUserData(userData); // Set the user data in state
     }
   }, [loading, data]);
-  
+
 
   const logout = (event) => {
     event.preventDefault();
@@ -73,38 +73,47 @@ function Header() {
     setEditProfileModalOpen(false);
   };
 
-    
+
   return (
-    <div className="fixed top-0 left-0 right-0">
+    <div>
       <div className="flex flex-row flex-wrap pb-4 bg-slate-200 pt-4 ">
-        <div className="flex lg:basis-2/3 basis-full text-4xl justify-center lg:justify-start lg:pe-20">My Course Planner</div>
-        <div className="flex lg:basis-1/3 basis-full justify-center lg:justify-end pt-5 pb-3 lg:pt-0 lg:pb-0 lg:pr-10">
+        <div className="flex basis-full text-4xl justify-center lg:pe-20">My Course Planner</div>
+        <div className="flex basis-full justify-center lg:justify-end pt-5 pb-3 lg:pt-0 lg:pb-0 lg:pr-10">
           <button className="border rounded px-4" id="logout-button" aria-label="logout" alt="Logout Button" onClick={logout}>Logout</button>
         </div>
       </div>
-
-      <div className="grid grid-cols-6 row border shadow bio-div">
-        <div className="col-span-5 bio-list">
-          <div className="grid grid-cols-10">
-            <div className="col-span-1">
-              <div className="pb-2 ">Name</div>
-              <div className="pb-2">Bio</div>
-              <div className="pb-2">Skills</div>
-              <div>Subjects</div>
-            </div>
-            <div className="col-span-9">
-              <div className="pb-2">{user.first_name + " "+ user.last_name}</div>
-              <div className="pb-2">{user.biography}</div>
-              <div className="pb-2">{skillsString}</div>
-              <div>{subjectsString}</div>
-            </div>
+      <div className="shadow bio-div">
+        <div className="grid grid-cols-6 row">
+          <div className="col-span-5 bio-list">
+            <table className="table-auto">
+              <tbody>
+                <tr>
+                <td className="inline-block align-top pr-5">Name:</td>
+                  <td>{user.first_name + " " + user.last_name}</td>
+                </tr>
+                <tr>
+                  <td className="inline-block align-top pr-5">Biography:</td>
+                  <td>{user.biography}</td>
+                </tr>
+                <tr>
+                <td className="inline-block align-top pr-5">Skills:</td>
+                  <td>{skillsString}</td>
+                </tr>
+                <tr>
+                <td className="inline-block align-top pr-5">Subjects:</td>
+                  <td>{subjectsString}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="flex justify-center col-span-12 lg:col-span-1 edit-profile">
+            <EditUserProfile isOpen={editProfileModalOpen} onCloseModal={handleCloseEditProfileModal} userData={user} />
+            <button className="px-4 py-1 border rounded" id="edit-profile-btn" aria-label="edit profile" alt="Edit Profile" onClick={handleOpenEditProfileModal}>Edit Profile</button>
           </div>
         </div>
-        <div className="flex flex-col space-y-4">
-          <AddSubject isOpen={addSubjectModalOpen} onCloseModal={handleCloseAddSubjectModal}/>
-          <button className="my-10 px-4 border rounded" id="add-subject-btn" aria-label="add a subject" alt="Add a Subject" onClick={handleOpenAddSubjectModal}>Add a Subject</button>
-          <EditUserProfile isOpen={editProfileModalOpen} onCloseModal={handleCloseEditProfileModal}  userData={user} />
-          <button className="my-10 px-4 border rounded" id="edit-profile-btn" aria-label="add a subject" alt="Add a Subject" onClick={handleOpenEditProfileModal}>Edit Profile</button>
+        <div className="grid justify-items-center">
+          <AddSubject isOpen={addSubjectModalOpen} onCloseModal={handleCloseAddSubjectModal} />
+          <button className="px-4 py-1 mb-2 border rounded" id="add-subject-btn" aria-label="add a subject" alt="Add a Subject" onClick={handleOpenAddSubjectModal}>Add a Subject</button>
         </div>
       </div>
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,58 @@
 import Subject from "../components/Subject";
 import Header from "../components/Header";
-
+import { useState, useEffect } from "react";
 
 function Home() {
+    const [headerHeight, setHeaderHeight] = useState(0);
+
+    useEffect(() => {
+        const updateHeaderHeight = () => {
+            // Check that the element with ID "header-content" exists
+            const header = document.getElementById("header-content");
+            if (header) {
+                // Update headerHeight with the offsetHeight of the header element
+                setHeaderHeight(header.offsetHeight);
+                console.log("Header height updated:", header.offsetHeight);
+            } else {
+                console.log("Header element not found");
+            }
+        };
+
+        // Call updateHeaderHeight on page load
+        updateHeaderHeight();
+
+        // Add event listener for window resize
+        window.addEventListener('resize', updateHeaderHeight);
+
+        // Clean up the event listener on component unmount
+        return () => {
+            window.removeEventListener('resize', updateHeaderHeight);
+            console.log("Event listener removed");
+        };
+    }, []);
+
+    console.log("Current header height:", headerHeight);
+
+    const mainStyle = {
+        marginTop: `${headerHeight}px`,
+        padding: '20px',
+        boxSizing: 'border-box',
+    };
 
     return (
-        <div className="flex justify-center home-div">
+        <div className="flex justify-center">
             <div>
-                <Header />
-                 <Subject />
+                <div className="fixed top-0 left-0 right-0" id="header-content">
+                    <Header />
+                </div>
+                <div id="main-content" style={mainStyle}>
+                    <Subject />
+                </div>
             </div>
-
         </div>
     );
 }
 
 export default Home;
+
+
