f1f14c44899861385ff507e0f78428437ea87ab7
YoonJae Kim <yoonjaekim3@gmail.com>
2023-11-14 21:36:28 -0600

    Fixed bio so that info shows up on screen

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,13 +32,11 @@ function Header() {
   }, [userId, refetch]);
    
   useEffect(() => {
-    console.log('abc')
-    console.log(data)
-    console.log(userId)
     if (userId && data) {
       console.log("User Data:", data);
       const userData = data.user || {};
       console.log("User:", userData); // Log the user object
+      setUserData(userData); // Set the user data in state
     }
   }, [loading, data]);
   
@@ -48,11 +46,19 @@ function Header() {
     Auth.logout();
   }
 
+  const subjectsArray = user.subjects || [];
+  const subjectNamesArray = subjectsArray.map(subject => subject.name);
+  const subjectsString = subjectNamesArray.join(', ');
+  const skillsArray = user.skills || [];
+  const skillsString = skillsArray.join(', ');
+
+
   const[openModal, setOpenModal] = useState(false);
   
   const handleClickEvent = () => {
     setOpenModal(true) }
 
+    
   return (
     <div className="fixed top-0 left-0 right-0">
       <div className="flex flex-row flex-wrap pb-4 bg-slate-200 pt-4 ">
@@ -62,7 +68,7 @@ function Header() {
         </div>
       </div>
 
-      <div className="grid grid-cols-7 row border shadow bio-div">
+      <div className="grid grid-cols-6 row border shadow bio-div">
         <div className="col-span-5 bio-list">
           <div className="grid grid-cols-10">
             <div className="col-span-1">
@@ -72,16 +78,12 @@ function Header() {
               <div>Subjects</div>
             </div>
             <div className="col-span-9">
-              <div className="pb-2"></div>
-              <div className="pb-2">r</div>
-              <div className="pb-2">placeholder</div>
-              <div>placeholder</div>
+              <div className="pb-2">{user.first_name + " "+ user.last_name}</div>
+              <div className="pb-2">{user.biography}</div>
+              <div className="pb-2">{skillsString}</div>
+              <div>{subjectsString}</div>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center col-span-12 lg:col-span-1">
-          {/* <AddSubject isOpen={openModal} onCloseModal={() => setOpenModal(false)} /> */}
-          <button className="my-10 px-4 border rounded" id="edit-user-btn" aria-label="edit user" alt="Edit User" onClick={handleClickEvent}>Edit Profile</button>
         </div>
         <div className="flex justify-center col-span-12 lg:col-span-1">
           <AddSubject isOpen={openModal} onCloseModal={() => setOpenModal(false)} />
