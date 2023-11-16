4562c52632d0ef7523e0b73c448f9fd3b0f79c10
Brian Klein <bklein1981@gmail.com>
2023-11-15 21:40:11 -0600

    updated cards

--- client/src/components/Course.jsx
+++ client/src/components/Course.jsx
@@ -3,38 +3,53 @@ import { useState } from "react";
 import editIcon from "../assets/images/edit_icon.svg";
 import deleteIcon from "../assets/images/delete_icon.svg";
 import EditCourse from "./EditCourse";
+import AddProject from './AddProject';
+import addIcon from '../assets/images/add_icon.svg';
 
 function Course(courseData) {
   const [openModal, setOpenModal] = useState(false);
+  const [openAddModal, setAddModal] = useState(false);
+
 
   const handleClickEvent = () => {
     setOpenModal(true)
   }
+  const addhandleClickEvent = () => {
+    setAddModal(true)
+  }
 
   const course = courseData.course
-
+ 
   return (
-    <Card className="max-w-sm">
-      <div className="grid grid-cols-10">
-        <div className="col-span-9 text-2xl	font-bold">{course.name}</div>
-        <div>
-          <div className="pb-2">
-            <EditCourse isOpen={openModal} onCloseModal={() => setOpenModal(false)} />
-            <button id="edit-course-btn" aria-label="edit-course" onClick={handleClickEvent}><img className="edit-subj-img" src={editIcon} alt="edit course" /></button>
+    <div>
+      <Card className="max-w-sm">
+        <div className="grid grid-cols-10">
+          <div className="col-span-9 text-2xl	font-bold">{course.name}</div>
+          <div>
+            <div className="pb-2">
+              <EditCourse isOpen={openModal} onCloseModal={() => setOpenModal(false)} />
+              <button id="edit-course-btn" aria-label="edit-course" onClick={handleClickEvent}><img className="edit-subj-img" src={editIcon} alt="edit course" /></button>
+            </div>
+            <div><button id="del-course-btn" aria-label="delete-course"><img className="delete-subj-img" src={deleteIcon} alt="delete course" /></button></div>
           </div>
-          <div><button id="del-course-btn" aria-label="delete-course"><img className="delete-subj-img" src={deleteIcon} alt="delete course" /></button></div>
         </div>
-      </div>
-      <div className="grid grid-cols-6">
-        <div className="col-span-2 text-lg font-medium text-gray-900">Start Date:</div>
-        <div className="col-span-2 text-lg font-medium text-gray-500">{!course.startDate ? "N/A" : course.startDate}</div>
-      </div>
-      <div className="grid grid-cols-6">
-        <div className="col-span-2 text-lg font-medium text-gray-900">End Date:</div>
-        <div className="col-span-2 text-lg font-medium text-gray-500">{!course.endDate ? "N/A" : course.endDate}</div>
-      </div>
-    </Card>
-
+        <div className="grid grid-cols-6">
+          <div className="col-span-2 text-lg font-medium text-gray-900">Start Date:</div>
+          <div className="col-span-2 text-lg font-medium text-gray-500">{!course.startDate ? "N/A" : course.startDate}</div>
+        </div>
+        <div className="grid grid-cols-6">
+          <div className="col-span-2 text-lg font-medium text-gray-900">End Date:</div>
+          <div className="col-span-2 text-lg font-medium text-gray-500">{!course.endDate ? "N/A" : course.endDate}</div>
+        </div>
+      </Card>
+      <Card className="max-w-sm">
+        <div className="flex items-center justify-between grid grid-cols-12">
+          <h5 className="col-span-11 text-xl font-bold leading-none text-gray-900 dark:text-white">Current Projects</h5>
+          <AddProject isOpen={openAddModal} onCloseModal={() => setAddModal(false)} />
+          <button className='col-span-1 justify-self-center' onClick={addhandleClickEvent}><img className='add-button' src={addIcon} aria-label="add project" alt="add project button" /></button>
+        </div>
+      </Card>
+    </div>
   );
 }
 
--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,20 +1,16 @@
-import { Card } from 'flowbite-react';
 import { useState } from 'react';
-import AddProject from './AddProject';
 import EditProject from './EditProject';
 
-import addIcon from '../assets/images/add_icon.svg';
+
 import editIcon from '../assets/images/edit_icon.svg';
 import deleteIcon from '../assets/images/delete_icon.svg';
 
 function Project(projectData) {
-  const [openAddModal, setAddModal] = useState(false);
+
 
   const [openEditModal, setEditModal] = useState(false);
 
-  const addhandleClickEvent = () => {
-    setAddModal(true)
-  }
+
   const edithandleClickEvent = () => {
     setEditModal(true)
   }
@@ -22,12 +18,7 @@ function Project(projectData) {
   const project = projectData.projects;
 
   return (
-    <Card className="max-w-sm">
-      <div className="flex items-center justify-between grid grid-cols-12">
-        <h5 className="col-span-11 text-xl font-bold leading-none text-gray-900 dark:text-white">Current Projects</h5>
-        <AddProject isOpen={openAddModal} onCloseModal={() => setAddModal(false)} />
-        <button className='col-span-1 justify-self-center' onClick={addhandleClickEvent}><img className='add-button' src={addIcon} aria-label="add project" alt="add project button" /></button>
-      </div>
+
       <div className="flow-root">
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           <li className="py-3 sm:py-4">
@@ -54,7 +45,7 @@ function Project(projectData) {
 
         </ul>
       </div>
-    </Card>
+    // </Card>
   );
 }
 
